6056f844846bf1219ad3e48142d94bd140442efe
Mukhammadyunus <dr.mukhammadyunus@gmail.com>
2025-12-09 13:44:30 +0100

    feat: add navigate function for clicking start button

--- src/pages/user/components/PostContent.jsx
+++ src/pages/user/components/PostContent.jsx
@@ -1,7 +1,15 @@
 import PostImages from "./PostImages";
 import PostFiles from "./PostFiles";
+import { useNavigate } from "react-router-dom";
 
 const PostContent = ({ item, itemType, images, files }) => {
+  const navigate = useNavigate();
+  const onStartTest = (e)=>{
+    e.stopPropagation()
+    navigate(
+      `/d/${item.data.desc.name}/test/${item.id}/start`
+    )
+  }
   return (
     <div>
       {itemType === "test" ? (
@@ -12,7 +20,7 @@ const PostContent = ({ item, itemType, images, files }) => {
           </div>
           <button
             className="ml-auto px-4 py-2 rounded bg-primary-blue text-white text-sm hover:bg-primary-blue/90 transition-colors duration-200 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 cursor-pointer"
-            onClick={(e) => e.stopPropagation()}
+            onClick={onStartTest}
           >
             Start
           </button>
