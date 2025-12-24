0e6470696c342545dc23a6fd0b440b49d0a84158
Mukhammadyunus <dr.mukhammadyunus@gmail.com>
2025-12-24 18:44:14 +0100

    feat: add reducers to initialize session, start session and to set submission

--- src/app/testSessionSlice.js
+++ src/app/testSessionSlice.js
@@ -27,9 +27,45 @@ const initialState = {
 const testSessionSlice = createSlice({
   name: "testSession",
   initialState,
-  reducers: {},
+  reducers: {
+    initializeSession: (state, action) => {
+      state.test.id = action.payload.test.id;
+      state.test.duration = action.payload.test?.duration ?? null;
+      state.currentIndex = 0;
+      state.submission = {};
+      state.questions = action.payload.questions;
+      state.meta.totalQuestions = action.payload.questions.length;
+      state.meta.answeredCount = 0;
+      state.meta.startedAt = null;
+
+      // state.meta.submittedAt = null
+      state.status = "ready";
+    },
+    startSession: (state) => {
+      state.meta.startedAt = Date.now();
+      state.currentIndex = 0;
+      state.status = "in_progress";
+    },
+    setSubmission: (state, action) => {
+      const id = action.payload.question_id;
+      const questionType = action.payload.question_type;
+      switch (questionType) {
+        case "mcq":
+          const optionId = action.payload.option_id;
+          if (Array.isArray(state.submission[id])) {
+            state.submission[id].push(optionId);
+          } else {
+            state.submission[id] = [optionId];
+          }
+          break;
+        case "code":
+          state.submission[id] = action.payload.code;
+          break;
+      }
+    },
+  },
 });
 
-export const { } =
+export const { initializeSession, startSession, setSubmission } =
   testSessionSlice.actions;
 export default testSessionSlice.reducer;
