bff022ed42f2ac9609fbfb24e3215c629cc500aa
Mukhammadyunus <dr.mukhammadyunus@gmail.com>
2025-12-27 18:24:18 +0100

    feat: complete question renderer

--- src/pages/user/test/QuestionRenderer.jsx
+++ src/pages/user/test/QuestionRenderer.jsx
@@ -1,9 +1,57 @@
-import React from 'react'
+import React, { lazy, Suspense, useMemo, useState } from "react";
+import { useSelector } from "react-redux";
+import Loading from "../../../components/Loading";
+import Toast from "../../../components/Toast";
+const RenderCodeQuestion = lazy(() => import("./RenderCodeQuestion"));
+const RenderMultiSelectMcqQuestion = lazy(() =>
+  import("./RenderMultiSelectMcqQuestion")
+);
+
+const renders = {
+  code: RenderCodeQuestion,
+  mcq: RenderMultiSelectMcqQuestion,
+};
 
 const QuestionRenderer = () => {
-  return (
-    <div>QuestionRenderer</div>
-  )
-}
+  const [error, setError] = useState({
+    hasError: false,
+    message: null,
+  });
 
-export default QuestionRenderer
+  const { questions, currentIndex, questionTypes } = useSelector((state) => state.testSession);
+
+  const question = useMemo(
+    () => questions[currentIndex],
+    [questions, currentIndex]
+  );
+
+  const QuestionComponent = useMemo(() => {
+    if (!questionTypes) return null;
+    const questionType = questionTypes[question.question_type_id]
+    return renders[questionType.type] ?? null;
+  }, [questionTypes, question]);
+
+  return (
+    <Suspense fallback={<Loading />}>
+      {QuestionComponent ? (
+        <>
+          <QuestionComponent onError={(err) => setError(err)} />
+          {error.hasError ? (
+            <Toast
+              message={error.message}
+              onClose={() => setError({ hasError: false, message: null })}
+              key={"question-renderer"}
+              type="error"
+            />
+          ) : null}
+        </>
+      ) : (
+        <div className="text-red-600 font-medium">
+          Something went wrong while rendering this question.
+        </div>
+      )}
+    </Suspense>
+  );
+};
+
+export default QuestionRenderer;
