1bb935eff1de3cd0c8d3d0f002d125b3dfc56549
Mukhammadyunus <dr.mukhammadyunus@gmail.com>
2026-01-02 12:17:24 +0100

    feat; implement an api query to check if use is logged in and to  dispatch it to setIsAuthenticated, and implement sessionStorage saving before navigating

--- src/pages/user/test/TestPage.jsx
+++ src/pages/user/test/TestPage.jsx
@@ -1,32 +1,39 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { TestInstructions } from "./TestInstructions";
 import { QuestionRunner } from "./QuestionRunner";
 import { ReviewAnswers } from "./ReviewAnswers";
 import { EditAnswer } from "./EditAnswer";
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import { TestResultPage } from "./TestResultPage";
 import { useEffect } from "react";
+import { useAmILoggedInQuery } from "../../../services/authApi";
+import { setIsAuthenticated } from "../../../app/authSlice";
 
 export const TestPage = () => {
   const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const location = useLocation()
   const { status, test } = useSelector((s) => s.testSession);
   const { isAuthenticated } = useSelector((s) => s.auth);
+  const { data: loginStatus } = useAmILoggedInQuery(undefined, {skip: isAuthenticated !== undefined});
   if (isAuthenticated === false && status !== "idle") {
-    navigate("/login");
+    sessionStorage.setItem("last-visit", location.pathname)
+    navigate("/login", {replace: true});
     return null;
   }
   useEffect(() => {
-  if (status === "idle" || status === "completed") return;
-
-  const handler = (e) => {
-    e.preventDefault();
-    e.returnValue = "";
-  };
-
-  window.addEventListener("beforeunload", handler);
-
-  return () => window.removeEventListener("beforeunload", handler);
-}, [status]);
+    if (loginStatus === undefined || isAuthenticated) return;
+    dispatch(setIsAuthenticated(loginStatus.isAuthenticated));
+  }, [loginStatus, dispatch]);
+  useEffect(() => {
+    if (status === "idle" || status === "completed") return;
+    const handler = (e) => {
+      e.preventDefault();
+      e.returnValue = "";
+    };
+    window.addEventListener("beforeunload", handler);
+    return () => window.removeEventListener("beforeunload", handler);
+  }, [status]);
   switch (status) {
     case "idle":
       return <TestInstructions />;
@@ -39,9 +46,8 @@ export const TestPage = () => {
     case "edit":
       return <EditAnswer />;
 
-
-      case 'completed':
-        return <TestResultPage />
+    case "completed":
+      return <TestResultPage />;
 
     default:
       return <TestInstructions />;
