b9769830c3210e23ac27041e5f418bc2f8099381
Mukhammadyunus <dr.mukhammadyunus@gmail.com>
2026-01-26 17:45:22 +0100

    fix: add min h and w for checkbox

--- src/pages/user/test/RenderMultiSelectMcqQuestion.jsx
+++ src/pages/user/test/RenderMultiSelectMcqQuestion.jsx
@@ -66,7 +66,7 @@ const RenderMultiSelectMcqQuestion = () => {
                     }`}
             >
               <div
-                className={`w-5 h-5 rounded border-2 flex items-center justify-center transition-colors
+                className={`w-5 h-5 min-w-5 min-h-5 rounded border-2 flex items-center justify-center transition-colors
                       ${
                         isSelected
                           ? "bg-indigo-600 border-indigo-600 dark:bg-indigo-500 dark:border-indigo-500"
